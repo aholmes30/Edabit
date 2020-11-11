9ee060a4560a33b8b66c4f815ddd6452ee26357c
Anna <amholmes1300@gmail.com>
2020-11-10 21:38:24 -0500

    added more edabit problems

---
+++ src/veryEasy/IsTheNumberLessThanOrEqualToZero.java
@@ -0,0 +1,29 @@
+package veryEasy;
+
+public class IsTheNumberLessThanOrEqualToZero {
+
+    public static boolean lessThanOrEqualToZero(int num) {
+
+        return num <= 0;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(lessThanOrEqualToZero(7));
+    }
+
+}
+
+
+
+
+/*
+Create a method that takes an integer as its only argument and returns true if it's less
+than or equal to zero, otherwise return false.
+
+Examples
+lessThanOrEqualToZero(5) ➞ false
+lessThanOrEqualToZero(0) ➞ true
+lessThanOrEqualToZero(-2) ➞ true
+ */
