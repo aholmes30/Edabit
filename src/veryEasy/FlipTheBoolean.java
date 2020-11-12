d88f7a11154c79753194ae235fc7f31039160385
Anna <amholmes1300@gmail.com>
2020-11-11 21:29:02 -0500

    added another edabit

---
+++ src/veryEasy/FlipTheBoolean.java
@@ -0,0 +1,23 @@
+package veryEasy;
+
+public class FlipTheBoolean {
+
+    public static boolean reverse(boolean b) {
+        return !b;
+    }
+
+    public static void main(String[] args) {
+
+        boolean b = false;
+        System.out.println(reverse(b));
+
+
+    }
+}
+
+/*
+Create a function that reverses a boolean value.
+Examples
+reverse(true) ➞ false
+reverse(false) ➞ true
+ */
