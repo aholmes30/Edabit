95cb8955a2d6a7ae318bc839319de355ae489ef0
Anna <amholmes1300@gmail.com>
2020-11-11 19:01:55 -0500

    added another edabit problem

---
+++ src/veryEasy/CheckIfAnIntegerIsDivisbleByFive.java
@@ -0,0 +1,16 @@
+package veryEasy;
+
+public class CheckIfAnIntegerIsDivisbleByFive {
+
+    public static boolean divisibleByFive(int num) {
+        return num % 5 == 0;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(divisibleByFive(15));
+
+
+    }
+}
