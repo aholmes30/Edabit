904c9b08ee6a434b6e999e520112e25cb0b4e0b9
Anna <amholmes1300@gmail.com>
2020-11-12 17:10:52 -0500

    edabit problems

---
+++ src/veryEasy/CorrectTheMistakes.java
@@ -0,0 +1,25 @@
+package veryEasy;
+
+public class CorrectTheMistakes {
+    public static int squared(int a) {
+        return a * a;
+
+    }
+
+    public static void main(String[] args) {
+        int a = 33;
+        System.out.println(squared(a));
+    }
+}
+
+/*
+Fix the code in the code tab to pass this challenge (only syntax errors).
+Look at the examples below to get an idea of what the function should do.
+Examples
+squared(5) ➞ 25
+squared(9) ➞ 81
+squared(100) ➞ 10000
+Notes
+READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+Don't overthink this challenge; it's not supposed to be hard.
+ */
