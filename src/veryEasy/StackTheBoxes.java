d5ef6fe1d20dcef4199548a1b93b045223faedf1
Anna <amholmes1300@gmail.com>
2020-11-12 20:48:28 -0500

    added edabit problem

---
+++ src/veryEasy/StackTheBoxes.java
@@ -0,0 +1,12 @@
+package veryEasy;
+
+public class StackTheBoxes {
+    public static int stackBoxes(int n) {
+        return n*n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(stackBoxes(7));
+
+    }
+}
