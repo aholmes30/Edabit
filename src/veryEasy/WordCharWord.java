5ac379b225454c0a630f8f3c3bad688ba4538990
Anna <amholmes1300@gmail.com>
2020-11-14 17:53:00 -0500

    new edabit problem in very easy

---
+++ src/veryEasy/WordCharWord.java
@@ -0,0 +1,14 @@
+package veryEasy;
+
+public class WordCharWord {
+
+    public static String add(String c, String str) {
+        //return data: String
+        //param: String c and String str
+
+        return str.replaceAll(" ", c);
+
+    }
+
+
+}
