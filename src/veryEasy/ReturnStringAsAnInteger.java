ebae020f783c00f492b39591bad1c8057970137d
Anna <amholmes1300@gmail.com>
2020-11-16 16:52:22 -0500

    edabit added

---
+++ src/veryEasy/ReturnStringAsAnInteger.java
@@ -0,0 +1,22 @@
+package veryEasy;
+
+public class ReturnStringAsAnInteger {
+
+    public static int stringInt(String str) {
+
+        return new Integer(Integer.parseInt(str));
+
+    }
+
+}
+
+/*
+Create a function that takes a string and returns it as an integer.
+Examples
+stringInt("6") ➞ 6
+stringInt("1000") ➞ 1000
+stringInt("12") ➞ 12
+Notes
+All numbers will be whole.
+All numbers will be positive.
+ */
