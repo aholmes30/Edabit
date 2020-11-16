690472855688802f66aaf7bfd6f4336f20dbf766
Anna <amholmes1300@gmail.com>
2020-11-16 16:56:43 -0500

    edabit problem

---
+++ src/veryEasy/CubeTheSquareRoot.java
@@ -0,0 +1,19 @@
+package veryEasy;
+
+public class CubeTheSquareRoot {
+
+    public static int cubeSquareRoot(int num) {
+        return (int) Math.pow(num, 1.5);
+    }
+
+}
+
+/*
+Create a function that takes a number as an argument and returns the square root of that number cubed.
+Examples
+cubeSquareRoot (81) ➞ 729
+cubeSquareRoot (1646089) ➞ 2111932187
+cubeSquareRoot (695556) ➞ 580093704
+Notes
+All numbers will evenly square root, so don't worry about decimal numbers.
+ */
