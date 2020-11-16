285b33f4faef6c6c55442341f82add8029d9e603
Anna <amholmes1300@gmail.com>
2020-11-16 16:32:55 -0500

    added a new edabit

---
+++ src/veryEasy/CharToASCII.java
@@ -0,0 +1,18 @@
+package veryEasy;
+
+public class CharToASCII {
+
+    public static int ctoa(char c) {
+        return c;
+    }
+
+}
+
+/*
+Create a function that returns the ASCII value of the passed in character.
+Examples
+ctoa('A') ➞ 65
+ctoa('m') ➞ 109
+ctoa('[') ➞ 91
+ctoa('\') ➞ 92
+ */
