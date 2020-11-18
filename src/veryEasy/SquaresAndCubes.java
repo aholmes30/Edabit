c8a78f6b17d801a77f1b03c8ab33869baeb4199f
Anna <amholmes1300@gmail.com>
2020-11-17 19:07:04 -0500

    added one edabit

---
+++ src/veryEasy/SquaresAndCubes.java
@@ -0,0 +1,22 @@
+package veryEasy;
+
+public class SquaresAndCubes {
+
+    public static boolean squaresAndCubes(int[] arr) {
+        return (Math.sqrt(arr[0]) * arr[0] == arr[1]);
+    }
+}
+
+/*
+Create a function that takes an array of two numbers and checks if the square root
+of the first number is equal to the cube root of the second number.
+
+Examples
+checkSquareAndCube([4, 8]) ➞ true
+
+checkSquareAndCube([16, 48]) ➞ false
+
+checkSquareAndCube([9, 27]) ➞ true
+Notes
+Remember to return either true or false.
+ */
