5c74a6616d5edb6e8afd095e680076140fed8b3e
Anna <amholmes1300@gmail.com>
2020-11-19 15:57:40 -0500

    added another problem

---
+++ src/veryEasy/AdditiveInverse.java
@@ -0,0 +1,19 @@
+package veryEasy;
+
+public class AdditiveInverse {
+    public static int[] additiveInverse(int[] arr) {
+    for (int i = 0; i < arr.length; i++){
+        arr[i] *= -1;
+    }
+		return arr;
+   }
+}
+
+/*
+A number added with its additive inverse equals zero. Create a function that
+returns an array of additive inverses.
+Examples
+additiveInverse([5, -7, 8, 3]) ➞ [-5, 7, -8, -3]
+additiveInverse([1, 1, 1, 1, 1]) ➞ [-1, -1, -1, -1, -1]
+additiveInverse([-5, -25, 35]) ➞ [5, 25, -35]
+ */
