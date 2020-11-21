d76583df1ba390f9e18686c39be38c428f01b318
Anna <amholmes1300@gmail.com>
2020-11-21 12:40:36 -0500

    one more edabit

---
+++ src/veryEasy/RemoveTheFirstAndLastCharacters.java
@@ -0,0 +1,26 @@
+package veryEasy;
+
+public class RemoveTheFirstAndLastCharacters {
+    public static String removeFirstLast(String str) {
+        //method name: removeFirstLast
+        //method type: 4 -> return type is String, params are string str
+        //use a for loop
+
+        if(str.length() > 2) {
+            return str.substring(1, str.length()-1);
+        }
+        return str;
+    }
+}
+
+
+/*
+Create a function that removes the first and last characters from a string.
+Examples
+removeFirstLast("hello") ➞ "ell"
+removeFirstLast("maybe") ➞ "ayb"
+removeFirstLast("benefit") ➞ "enefi"
+removeFirstLast("a") ➞ "a"
+Notes
+If the string is 2 or fewer characters long, return the string itself (See example #4).
+ */
