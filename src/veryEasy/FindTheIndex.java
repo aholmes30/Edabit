04fd9a1a3d8a0ff0369ff4e9d478d237d0be939a
Anna <amholmes1300@gmail.com>
2020-11-24 19:30:24 -0500

    edabit

---
+++ src/veryEasy/FindTheIndex.java
@@ -0,0 +1,24 @@
+package veryEasy;
+
+public class FindTheIndex {
+
+    public static int findIndex(String[] arr, String str) {
+        for (int i = 0; i < arr.length; i++) {
+            if (str.equals(arr[i]))
+                return i;
+        }
+        return -1;
+
+    }
+
+}
+
+/*
+Create a function that takes an array and a string as arguments and return the index of the string.
+
+Examples
+findIndex(new String[]{"hi", "edabit", "fgh", "abc"}, "fgh") ➞ 2
+findIndex(new String[]{"Red", "blue", "Blue", "Green"}, "blue") ➞ 1
+findIndex(new String[]{"a", "g", "y", "d"}, "d") ➞ 3
+findIndex(new String[]{"Pineapple", "Orange", "Grape", "Apple"}, "Pineapple") ➞ 0
+ */
