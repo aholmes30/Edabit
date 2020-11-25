42004bc58cbbc6ad7e0911199449db34a56c351b
Anna <amholmes1300@gmail.com>
2020-11-25 16:31:08 -0500

    added another edabit

---
+++ src/veryEasy/ACircleAndTwoSquares.java
@@ -0,0 +1,26 @@
+package veryEasy;
+
+public class ACircleAndTwoSquares {
+
+    public static int squareAreasDifference(int r) {
+        return 2 * r * r;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(squareAreasDifference(6));
+    }
+}
+
+
+
+
+/*
+Create a function, that takes an integer (radius of the circle) and returns the difference of the areas of the two squares.
+Examples
+squareAreasDifference(5) ➞ 50
+squareAreasDifference(6) ➞ 72
+squareAreasDifference(7) ➞ 98
+Notes
+Use only positive integer parameters.
+ */
