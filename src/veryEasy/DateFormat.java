03a71514f71469e1787161ff62889d5c620ea438
Anna <amholmes1300@gmail.com>
2020-11-25 16:39:23 -0500

    added edabit

---
+++ src/veryEasy/DateFormat.java
@@ -0,0 +1,20 @@
+package veryEasy;
+
+public class DateFormat {
+
+    public static String formatDate(String date) {
+        String part[] = date.split("/");
+        return part[2] + part[1] + part[0];
+    }
+}
+
+/*
+Create a function that converts a date formatted as MM/DD/YYYY to YYYYDDMM.
+
+Examples
+formatDate("11/12/2019") ➞ "20191211"
+formatDate("12/31/2019") ➞ "20193112"
+formatDate("01/15/2019") ➞ "20191501"
+Notes
+Return value should be a string.
+ */
