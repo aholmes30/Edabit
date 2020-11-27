914163fea0cb05d40cab57b8cd6707c5482a24d8
Anna <amholmes1300@gmail.com>
2020-11-26 23:44:57 -0500

    edabit

---
+++ src/veryEasy/ReverseAndCapitalize.java
@@ -0,0 +1,22 @@
+package veryEasy;
+
+public class ReverseAndCapitalize {
+    public static String reverseCapitalize(String str) {
+        String s ="";
+        for(int i=str.length()-1;i>=0;i--){
+            s +=str.toUpperCase().charAt(i);
+        }
+        return s;
+    }
+}
+
+/*
+Create a function that takes a string of lowercase characters and returns that string reversed and in upper case.
+
+Examples
+reverseCapitalize("abc") ➞ "CBA"
+
+reverseCapitalize("hellothere") ➞ "EREHTOLLEH"
+
+reverseCapitalize("input") ➞ "TUPNI"
+ */
