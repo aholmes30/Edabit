58a8980712a6c9a2c417a6d018dbc4c54486db12
Anna <amholmes1300@gmail.com>
2020-11-26 23:32:46 -0500

    added another edabit problem

---
+++ src/veryEasy/SimilarBread.java
@@ -0,0 +1,35 @@
+package veryEasy;
+
+public class SimilarBread {
+
+    public static boolean hasSameBread(String[] a, String[] b) {
+        //method name: hasSameBread
+        //method type: 4 -> return data is boolean, param is string[] a and string[] b
+
+        if(a[0] == b[0] && a[2] == b[2]){
+            return true;
+        }
+        return false;
+    }
+}
+
+/*
+Given two arrays, which represent two sandwiches, return whether both sandwiches use
+the same type of bread. The bread will always be found at the start and end of the array.
+
+Examples
+hasSameBread(
+  ["white bread", "lettuce", "white bread"],
+  ["white bread", "tomato", "white bread"]
+) ➞ true
+
+hasSameBread(
+  ["brown bread", "chicken", "brown bread"],
+  ["white bread", "chicken", "white bread"]
+) ➞ false
+
+hasSameBread(
+  ["toast", "cheese", "toast"],
+  ["brown bread", "cheese", "toast"]
+) ➞ false
+ */
