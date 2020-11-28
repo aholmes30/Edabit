aad3d13479a0c8d77706fd758d676292324699ff
Anna <amholmes1300@gmail.com>
2020-11-28 17:51:56 -0500

    edabit added

---
+++ src/veryEasy/GetWordCount.java
@@ -0,0 +1,17 @@
+package veryEasy;
+
+public class GetWordCount {
+
+    public static int countWords(String s) {
+        return s.split(" ").length;
+    }
+}
+
+/*
+Create a method that takes a string and returns the word count. The string will be a sentence.
+
+Examples
+countWords("Just an example here move along") ➞ 6
+countWords("This is a test") ➞ 4
+countWords("What an easy task, right") ➞ 5
+ */
