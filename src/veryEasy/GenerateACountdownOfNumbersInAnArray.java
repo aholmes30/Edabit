5088948dfc11521af8e9c717985f055d3f829e63
Anna <amholmes1300@gmail.com>
2020-11-27 20:23:26 -0500

    edabit added

---
+++ src/veryEasy/GenerateACountdownOfNumbersInAnArray.java
@@ -0,0 +1,28 @@
+package veryEasy;
+
+public class GenerateACountdownOfNumbersInAnArray {
+
+    public static int[] countdown(int start) {
+        //method name: countdown
+        //method type: 4 -> return data is int[], params are int start
+
+        int[] result = new int[start+1];
+        for(int i = start; i >= 0; i--) {
+            result[start-i] = i;
+        }
+        return result;
+    }
+}
+
+
+/*
+Create a function that takes a number as an argument and returns an array
+of numbers counting down from this number to zero.
+
+Examples
+countdown(5) ➞ [5, 4, 3, 2, 1, 0]
+countdown(1) ➞ [1, 0]
+countdown(0) ➞ [0]
+Notes
+The argument will always be greater than or equal to zero.
+ */
