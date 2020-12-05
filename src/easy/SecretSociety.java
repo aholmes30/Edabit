26380b35e20ea478a7671c3e839e934e29b1d2ac
Anna <amholmes1300@gmail.com>
2020-12-05 18:45:57 -0500

    added edabit

---
+++ src/easy/SecretSociety.java
@@ -0,0 +1,30 @@
+package easy;
+
+import java.util.Arrays;
+
+public class SecretSociety {
+
+        public static String societyName(String[] friends) {
+            String s = "";
+            Arrays.sort(friends);
+
+            for(int i=0; i<friends.length; i++)
+            {
+                s += friends[i].charAt(0);
+            }
+            return s;
+        }
+}
+
+/*
+A group of friends have decided to start a secret society. The name will be the first letter of
+each of their names, sorted in alphabetical order.
+
+Create a function that takes in an array of names and returns the name of the secret society.
+Examples
+societyName(["Adam", "Sarah", "Malcolm"]) ➞ "AMS"
+societyName(["Harry", "Newt", "Luna", "Cho"]) ➞ "CHLN"
+societyName(["Phoebe", "Chandler", "Rachel", "Ross", "Monica", "Joey"]) ➞ "CJMPRR"
+Notes
+The secret society's name should be entirely uppercased.
+ */
