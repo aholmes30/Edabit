9cd66d594a5adf850891e52356b3159b4bb39caa
Anna <amholmes1300@gmail.com>
2020-12-09 21:51:57 -0500

    added edabit

---
+++ src/easy/LargestSwap.java
@@ -0,0 +1,35 @@
+package easy;
+
+public class LargestSwap {
+
+    public static boolean largestSwap(int num) {
+
+        int firstDigit = num / 10;
+        int secondDigit = num % 10;
+        int swapped_digit = (secondDigit * 10) + firstDigit;
+
+        if(num == swapped_digit) {
+            return true;
+        }
+        if(num > swapped_digit) {
+            return true;
+        }
+        return false;
+    }
+}
+
+/*
+Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+To illustrate:
+largestSwap(27) ➞ false
+largestSwap(43) ➞ true
+If 27 is our input, we should return false because swapping the digits gives us 72, and 72 > 27.
+On the other hand, swapping 43 gives us 34, and 43 > 34.
+
+Examples
+largestSwap(14) ➞ false
+largestSwap(53) ➞ true
+largestSwap(99) ➞ true
+Notes
+Numbers with two identical digits (third example) should yield true (you can't do better).
+ */
