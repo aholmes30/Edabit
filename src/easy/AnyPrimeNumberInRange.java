9b1fd86810bc1a959508fff930f31916651b6963
Anna <amholmes1300@gmail.com>
2020-12-15 10:33:31 -0500

    updated edabit

--- src/easy/AnyPrimeNumberInRange.java
+++ src/easy/AnyPrimeNumberInRange.java
@@ -19,6 +19,11 @@ public class AnyPrimeNumberInRange {
         return false;
     }
 
+    public static void main(String[] args) {
+
+        System.out.println(PrimeNumberInRange(27, 80));
+    }
+
 }
 
 /*
