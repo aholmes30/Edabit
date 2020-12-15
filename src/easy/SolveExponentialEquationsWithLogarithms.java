b7292664b5ca475719bb5f4a15a9acca9d9608eb
Anna <amholmes1300@gmail.com>
2020-12-15 10:24:31 -0500

    added edabit to easy

---
+++ src/easy/SolveExponentialEquationsWithLogarithms.java
@@ -0,0 +1,27 @@
+package easy;
+
+public class SolveExponentialEquationsWithLogarithms {
+
+
+    public static int solveForExp(int a, int b) {
+
+        int c = 0;
+        for(int x = 0; c==0; x++)
+            if (b == Math.pow(a, x))
+                return x;
+        return 0;
+    }
+
+}
+
+/*
+Create a function that takes a number a and finds the missing exponent x
+so that a when raised to the power of x is equal to b.
+
+Examples
+solveForExp(4, 1024) ➞ 5
+solveForExp(2, 1024) ➞ 10
+solveForExp(9, 3486784401) ➞ 10
+Notes
+a is raised to the power of what in order to equal b?
+ */
