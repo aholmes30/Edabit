308dd7e911ba7baa0e2ceca728b1d25206473df9
Anna <amholmes1300@gmail.com>
2020-12-16 10:39:41 -0500

    added edabit

---
+++ src/easy/StringOrInteger.java
@@ -0,0 +1,26 @@
+package easy;
+
+public class StringOrInteger {
+
+
+    public static String intOrString(Object var) {
+        if(var instanceof String)
+            return "str";
+        else
+            return "int";
+
+    }
+
+}
+
+/*
+Create a function that checks if the argument is an integer or a string.
+Return "int" if it's an integer and "str" if it's a string.
+
+Examples
+intOrString(8) ➞ "int"
+intOrString("Hello") ➞ "str"
+intOrString(9843532) ➞ "int"
+Notes
+Input will either be an integer or a string.
+ */
