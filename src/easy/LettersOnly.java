1a95b138406c414e1f9f86fbbab6f5e0fe60973a
Anna <amholmes1300@gmail.com>
2020-12-16 10:42:21 -0500

    added edabit

---
+++ src/easy/LettersOnly.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class LettersOnly {
+
+    public static String lettersOnly(String str) {
+        //return data String, param String str
+
+        return str.replaceAll("[_;:|!@#\\]\\[$%^&*().\\-~<>'+={}?,`1234567890//]", "");
+
+    }
+}
+
+/*
+Write a function that removes any non-letters from a string, returning a well-known film title.
+Examples
+lettersOnly("R!=:~0o0./c&}9k`60=y") ➞ "Rocky"
+lettersOnly("^,]%4B|@56a![0{2m>b1&4i4") ➞ "Bambi"
+lettersOnly("^U)6$22>8p).") ➞ "Up"
+ */
