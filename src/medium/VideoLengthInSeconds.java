79f33ca40b82e982adb8160ba88a66558bb3f127
Anna <amholmes1300@gmail.com>
2020-12-22 16:30:07 -0500

    added edabit

---
+++ src/medium/VideoLengthInSeconds.java
@@ -0,0 +1,34 @@
+package medium;
+
+public class VideoLengthInSeconds {
+
+
+    public static int minutesToSeconds(String tm) {
+        String [] arr = tm.split(":");
+        int minutes = Integer.parseInt(arr[0]);
+        int seconds = Integer.parseInt(arr[1]);
+        int answer = 0;
+        if(seconds >= 60){
+            return -1;
+        }
+        else {
+            answer = minutes * 60 + seconds;
+        }
+        return answer;
+    }
+
+}
+
+/*
+You are given the length of a video in minutes. The format is mm:ss (e.g.: "02:54").
+Create a function that takes the video length and return it in seconds.
+Examples
+minutesToSeconds("01:00") ➞ 60
+minutesToSeconds("13:56") ➞ 836
+minutesToSeconds("10:60") ➞ -1
+Notes
+The video length is given as a string.
+If the number of seconds is 60 or over, return -1 (see example #3).
+You may get a number of minutes over 99 (e.g. "121:49" is perfectly valid).
+ */
+
