38b4ad387ac862564d519f023cbcad879d0dedb6
Anna <amholmes1300@gmail.com>
2020-12-22 16:32:25 -0500

    added edabit

---
+++ src/medium/ReverseTheCase.java
@@ -0,0 +1,33 @@
+package medium;
+
+public class ReverseTheCase {
+
+    public static String reverseCase(String str) {
+        //return data: String
+        // param: String str
+
+        char[] chars = str.toCharArray();		//create char array from String
+
+        for(int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if(Character.isUpperCase(c)) {
+                chars[i] = Character.toLowerCase(c);
+            }
+            else if (Character.isLowerCase(c)) {
+                chars[i] = Character.toUpperCase(c);
+            }
+        }
+        return new String(chars);
+    }
+
+}
+
+/*
+Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
+
+Examples
+reverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
+reverseCase("MANY THANKS") ➞ "many thanks"
+reverseCase("sPoNtAnEoUs") ➞ "SpOnTaNeOuS"
+ */
+
