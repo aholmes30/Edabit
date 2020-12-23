8e2e41a4f5ca5575c1b919a168ccda11e4a1251a
Anna <amholmes1300@gmail.com>
2020-12-23 17:25:05 -0500

    added edabit

---
+++ src/medium/NextPrime.java
@@ -0,0 +1,28 @@
+package medium;
+
+public class NextPrime {
+
+    public static int nextPrime(int num) {
+        for(int i=num; i<50; i++) {
+            boolean isPrime = true;
+            for(int j=2; j<num; j++) {
+                if(i%j==0) isPrime = false;
+            }
+            if(isPrime) return i;
+        }
+        return 0;
+    }
+
+
+}
+
+/*
+Given an integer, create a function that returns the next prime.
+If the number is prime, return the number itself.
+
+Examples
+nextPrime(12) ➞ 13
+nextPrime(24) ➞ 29
+nextPrime(11) ➞ 11
+// 11 is a prime, so we return the number itself.
+ */
