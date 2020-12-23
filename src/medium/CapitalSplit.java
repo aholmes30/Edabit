32e6b66e25deab01da335ca1c040855e48b674f5
Anna <amholmes1300@gmail.com>
2020-12-23 17:16:58 -0500

    updated edabit

--- src/medium/CapitalSplit.java
+++ src/medium/CapitalSplit.java
@@ -10,6 +10,11 @@ public class CapitalSplit {
         return a;
     }
 
+    public static void main(String[] args) {
+
+        System.out.println(capSpace("Tell your mom I said hello"));
+    }
+
 
 }
 
