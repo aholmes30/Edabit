5d9a5a1dd8e8efd34c8981e8f394a73c9df6c00c
Anna <amholmes1300@gmail.com>
2020-12-23 17:22:29 -0500

    updated edabit

--- src/medium/InstantJazz.java
+++ src/medium/InstantJazz.java
@@ -2,13 +2,19 @@ package medium;
 
 public class InstantJazz {
 
-    public static String[]  jazzify(String[] arr) {
+    public static String[]jazzify(String[] arr) {
         for(int i = 0; i < arr.length; i++){
             if(arr[i].charAt(arr[i].length() -1) != '7'){
                 arr[i] = (arr[i] + "7");
             }
         }
         return arr;
+    }
+
+    public static void main(String[] args) {
+
+        String[] arr = {"A", "B", "C"};
+        System.out.println(jazzify(arr));
     }
 
 }
