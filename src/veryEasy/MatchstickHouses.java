373ffcfe7af2a870ff0db5fbb5cb52d13e32df02
Anna <amholmes1300@gmail.com>
2020-12-25 11:58:29 -0500

    updated edabit

--- src/veryEasy/MatchstickHouses.java
+++ src/veryEasy/MatchstickHouses.java
@@ -11,6 +11,11 @@ public class MatchstickHouses {
             return step*5+1;
         }
     }
+
+    public static void main(String[] args) {
+
+        System.out.println(matchHouses(4));
+    }
 }
 
 /*
