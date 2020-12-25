ec55d3a5ae68ac041df607424af5c822f86e39a0
Anna <amholmes1300@gmail.com>
2020-12-25 12:03:25 -0500

    updated edabit

--- src/veryEasy/OwoifiedSentence.java
+++ src/veryEasy/OwoifiedSentence.java
@@ -11,6 +11,11 @@ public class OwoifiedSentence {
         return replace + addedWord;
 
     }
+
+    public static void main(String[] args) {
+
+        System.out.println(owofied("Hello, I like owls."));
+    }
 }
 
 /*
