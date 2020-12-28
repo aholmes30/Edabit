fd5c321bc68bb3a7f84887b51ab813399b0b64cb
Anna <amholmes1300@gmail.com>
2020-12-28 16:55:32 -0500

    updated edabit

--- src/easy/SpacesBetweenEachCharacter.java
+++ src/easy/SpacesBetweenEachCharacter.java
@@ -11,6 +11,11 @@ public class SpacesBetweenEachCharacter {
         }
         return result + str.substring(str.length()-1);
     }
+
+    public static void main(String[] args) {
+
+        System.out.println(spaceMeOut("hello"));
+    }
 }
 
 /*
