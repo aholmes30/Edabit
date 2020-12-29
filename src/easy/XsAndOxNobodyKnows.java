68803c494ad0572fc539b5f5b5673ec0fefac09d
Anna <amholmes1300@gmail.com>
2020-12-29 09:33:57 -0500

    updated edabit

--- src/easy/XsAndOxNobodyKnows.java
+++ src/easy/XsAndOxNobodyKnows.java
@@ -18,6 +18,11 @@ public class XsAndOxNobodyKnows {
 
             return x == o;
         }
+
+    public static void main(String[] args) {
+
+        System.out.println(getXO("xoxoxoxox"));
+    }
     }
 
 
