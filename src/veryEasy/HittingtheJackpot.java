7dd2011dd860c92d7d27c217284791d52dbd4987
Anna <amholmes1300@gmail.com>
2021-01-01 17:49:35 -0500

    updated edabit

--- src/veryEasy/HittingtheJackpot.java
+++ src/veryEasy/HittingtheJackpot.java
@@ -11,6 +11,12 @@ public class HittingtheJackpot {
         return true;
     }
 
+    public static void main(String[] args) {
+
+        String[] result = {"&&", "&", "&&&", "&&&&"};
+        System.out.println(testJackpot(result));
+    }
+
 
 }
 
