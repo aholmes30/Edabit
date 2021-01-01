8fb8c0547bbf9deccffaeeb6f4b1f126d17d2094
Anna <amholmes1300@gmail.com>
2021-01-01 17:43:28 -0500

    updated edabit

--- src/veryEasy/GetWordCount.java
+++ src/veryEasy/GetWordCount.java
@@ -5,6 +5,11 @@ public class GetWordCount {
     public static int countWords(String s) {
         return s.split(" ").length;
     }
+
+    public static void main(String[] args) {
+
+        System.out.println(countWords("Hello World"));
+    }
 }
 
 /*
