cd5d39ef72dc136d454d0ccd0f7eface6f5ed618
Anna <amholmes1300@gmail.com>
2021-01-01 17:41:31 -0500

    updated edabit

--- src/veryEasy/GenerateACountdownOfNumbersInAnArray.java
+++ src/veryEasy/GenerateACountdownOfNumbersInAnArray.java
@@ -12,6 +12,10 @@ public class GenerateACountdownOfNumbersInAnArray {
         }
         return result;
     }
+
+    public static void main(String[] args) {
+        System.out.println(countdown(5));
+    }
 }
 
 
